51f9591efd1e30a894fd8e127832dc9b7e24931f
Manuele Rusci <manuele.rusci@unibo.it>
2020-06-04 14:56:35 +0200

    added mobilenetv3 among the benchmarks

--- main.h
+++ main.h
@@ -52,6 +52,9 @@
 #elif MODEL_ID==16
 	#include "mobilenet_v2_1_0_224_quant.h"
 	#include "mobilenet_v2_1_0_224_quantKernels.h"
+#elif MODEL_ID==17
+	#include "mobilenet_v3_large_1_0_224_quant.h"
+	#include "mobilenet_v3_large_1_0_224_quantKernels.h"
 #endif
 
 #include "Gap.h"
