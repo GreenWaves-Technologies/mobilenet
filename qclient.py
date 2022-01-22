70d7f40795471b4f072a313b1fb9f7c3de18cdeb
colinski <csamplawski@Colins-MacBook-Pro.local>
2022-01-22 14:56:17 -0500

    fixed queue based client

--- qclient.py
+++ qclient.py
@@ -3,7 +3,6 @@ import socket
 from pickle import dumps, loads
 from multiprocessing import Queue
 
-
 def recvall(sock, TARGET_SIZE=4096, BUFFER_SIZE=4096):
     data = b''
     while True:
@@ -23,25 +22,13 @@ class QClient:
         
     def run(self):
         while True:
-            message, num_bytes = self.get() 
-            self.sock.send(message)
+            message, num_bytes = self.Qin.get()
+            self.sock.sendall(message)
             buff = recvall(self.sock, num_bytes, self.BUFFER_SIZE)
-            self.put(buff)
-    
+            self.Qout.put(buff)
+   
     def put(self, x):
         self.Qin.put(x)
 
     def get(self):
         return self.Qout.get()
-            
-        
-# if __name__ == '__main__':
-    # TCP_IP = '192.168.0.124'
-    # TCP_PORT = 5000
-    # BUFFER_SIZE = 1024
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # sock.connect((TCP_IP, TCP_PORT))
-    # root = Tk.Tk()
-    # gui = GUI(root, sock)
-    # root.after(200, gui.do_refresh) 
-    # root.mainloop()
