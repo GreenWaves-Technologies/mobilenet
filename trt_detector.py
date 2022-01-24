aff38efa98eb3cfd8e3af1fc8a95f9cf681c99b8
Colin Samplawski <csamplawski@cs.umass.edu>
2022-01-23 21:10:07 -0500

    detector returns a fixed number of dets, padding zeros if needed

--- trt_detector.py
+++ trt_detector.py
@@ -12,7 +12,7 @@ class TRTDetector(object):
         with open(self.model, 'rb') as f, trt.Runtime(self.trt_logger) as runtime:
             return runtime.deserialize_cuda_engine(f.read())
 
-    def __init__(self, model, category_num=80, cuda_ctx=None):
+    def __init__(self, model, category_num=80, num_dets=50, cuda_ctx=None):
         """Initialize TensorRT plugins, engine and conetxt."""
         self.model = model
         self.category_num = category_num
@@ -23,6 +23,7 @@ class TRTDetector(object):
         self.trt_logger = trt.Logger(trt.Logger.INFO)
         self.engine = self._load_engine()
         self.input_shape = get_input_shape(self.engine)
+        self.num_dets = num_dets
         self.timings = []
 
         try:
@@ -61,13 +62,13 @@ class TRTDetector(object):
         # scale_y = img_h / input_shape[0]
         scale_x, scale_y = 1, 1
         
-        detections = outputs[0].reshape(1, -1, 7)[0] 
-        x1, y1, x2, y2 = np.split(detections[:, 0:4], 4, axis=-1)
+        dets = outputs[0].reshape(1, -1, 7)[0] 
+        x1, y1, x2, y2 = np.split(dets[:, 0:4], 4, axis=-1)
         #x, y, w, h = (x1 + x2)/2, (y1 + y2)/2, (x2 - x1), (y2 - y1)
         x, y, w, h = x1, y1, x2 - x1 + 1, y2 - y1 + 1
         x, w = x * scale_x, w * scale_x
         y, h = y * scale_y, h * scale_y
-        detections[:, 0:4] = np.concatenate([x,y,w,h], axis=-1)
+        dets[:, 0:4] = np.concatenate([x,y,w,h], axis=-1)
         
         #conf_scores = class_scores[..., -1][:, np.newaxis] 
         #class_scores = class_scores[..., 0:80]
@@ -75,41 +76,48 @@ class TRTDetector(object):
         #class_idx = np.argmax(class_scores, axis=-1)[:, np.newaxis] 
         #class_prob = np.amax(class_scores, axis=-1)[:, np.newaxis] 
 
-        #detections = np.concatenate([pred_bbox, conf_scores, class_idx, class_prob], axis=-1)
-        detections  = detections[detections[:, 4] * detections[:, 6] >= 1e-2]
-        scores = detections[:, 4] * detections[:, 6]
-        classes = detections[:, 5]
+        #dets = np.concatenate([pred_bbox, conf_scores, class_idx, class_prob], axis=-1)
+        dets  = dets[dets[:, 4] * dets[:, 6] >= 1e-2]
+        scores = dets[:, 4] * dets[:, 6]
+        classes = dets[:, 5]
 
-        nms_detections = np.zeros((0, 7), dtype=detections.dtype)
-        for class_id in set(detections[:, 5]):
-            idxs = np.where(detections[:, 5] == class_id)
-            cls_detections = detections[idxs]
-            keep = nms_boxes(cls_detections, 0.5)
-            nms_detections = np.concatenate([nms_detections, cls_detections[keep]], axis=0)
-        xx = nms_detections[:, 0].reshape(-1, 1)
-        yy = nms_detections[:, 1].reshape(-1, 1)
-        ww = nms_detections[:, 2].reshape(-1, 1)
-        hh = nms_detections[:, 3].reshape(-1, 1)
+        nms_dets = np.zeros((0, 7), dtype=dets.dtype)
+        for class_id in set(dets[:, 5]):
+            idxs = np.where(dets[:, 5] == class_id)
+            cls_dets = dets[idxs]
+            keep = nms_boxes(cls_dets, 0.5)
+            nms_dets = np.concatenate([nms_dets, cls_dets[keep]], axis=0)
+        xx = nms_dets[:, 0].reshape(-1, 1)
+        yy = nms_dets[:, 1].reshape(-1, 1)
+        ww = nms_dets[:, 2].reshape(-1, 1)
+        hh = nms_dets[:, 3].reshape(-1, 1)
         boxes = np.concatenate([xx, yy, xx+ww, yy+hh], axis=1) + 0.5
-        scores = nms_detections[:, 4] * nms_detections[:, 6]
-        classes = nms_detections[:, 5]
+        scores = nms_dets[:, 4] * nms_dets[:, 6]
+        classes = nms_dets[:, 5]
         
         # clip x1, y1, x2, y2 within original image
         boxes[:, [0, 2]] = np.clip(boxes[:, [0, 2]], 0, img_w-1)
         boxes[:, [1, 3]] = np.clip(boxes[:, [1, 3]], 0, img_h-1)
 
-        detections = np.concatenate((
+        dets = np.concatenate((
             boxes, 
             scores[:, np.newaxis] * 100, #[0,1] -> [0,100]
             classes[:, np.newaxis]), 
         axis=1)
 
-        detections = detections.astype(np.uint16)
+        dets = dets.astype(np.uint16)
+        # if len(dets) > self.num_dets:
+
+        idx = np.argsort(-dets[:, -2]) #sort by score
+        dets = dets[idx]
+        dets = dets[0:self.num_dets]
+        zeros = np.zeros((self.num_dets - len(dets), 6), dtype=dets.dtype)
+        dets = np.concatenate((dets, zeros), axis=0)
         
         # if self.cuda_ctx:
             # self.cuda_ctx.pop()
 
-        return detections
+        return dets
         
         
     def detect(self, img):
