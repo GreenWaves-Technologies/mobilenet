8930a582460d4096f23462364877815c4c95285c
Colin Samplawski <csamplawski@cs.umass.edu>
2022-02-07 22:19:45 -0500

    updated test server, works after meeting with Ben

--- test_server.py
+++ test_server.py
@@ -7,16 +7,14 @@ import subprocess
 from trt_detector import TRTDetector
 from utils.network import buff2numpy
 
-def recvall(sock, target_size=4096, buffer_size=4096):
+def recvall(conn, target_size=4096, buffer_size=4096):
     data = b''
-    print(len(data), end=' ')
     while True:
-        part = sock.recv(buffer_size)
+        part = conn.recv(buffer_size)
         if not part:
             return None
         data += part
-        print(len(data), end=' ')
-        if len(data) == target_size:
+        if len(data) >= target_size:
             break
     return data
 
@@ -24,46 +22,43 @@ def recvall(sock, target_size=4096, buffer_size=4096):
 class NanoServer:
     def __init__(self, 
                  TCP_IP='0.0.0.0', TCP_PORT=80,
-                 buffer_size=32,
+                 buffer_size=1000,
     ):
         self.TCP_IP = TCP_IP
         self.TCP_PORT = TCP_PORT
         self.buffer_size = buffer_size
-        self.num_bytes = 32*5
+        self.num_bytes = 250
         # self.num_bytes = 8*28*28
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((self.TCP_IP, self.TCP_PORT))
+        self.sock.settimeout(10)
 
     def run(self):
         print("nano server started")
-        
         self.sock.listen()
         conn, addr = self.sock.accept()
+        total_len = 0
         print(addr)
-        while True:
-            message = recvall(conn, self.num_bytes, self.buffer_size)
-            if not message:
-                break
-
-            print(message)
-            print(len(message))
+        data = b''
+        seq_num = 0
+        packet_size = 1460
+        while len(data) != 1000 * packet_size:
+            while True:
+                part = conn.recv(packet_size)
+                data += part
+                if len(data) >= packet_size:
+                    message = data[0:packet_size]
+                    data = data[packet_size:]
+                    total_len += len(message)
+                    remote_seq_num = message[0]
+                    assert remote_seq_num == seq_num
+                    seq_num += 1
+                    seq_num = seq_num % 256
+                    if total_len == 1000 * packet_size:
+                        print(message[0], len(message), total_len)
+                    break
             
-            # print('nano: got message of len', len(message))
-            # channels = buff2numpy(message, dtype=np.int8)
-            # channels = channels.reshape(8, 28, 28)
-            # channels = channels.astype(np.float32)
-            # channels = (channels - -128) * 0.02352941
-            
-            # z_channels = np.zeros([32-8, 28, 28], dtype=np.float32)
-            # channels = np.concatenate([channels, z_channels], axis=0)
-            # channels = np.expand_dims(channels, axis=0) #1 C H W
-            # dets = self.detector.detect(channels)
-            # for det in dets:
-                # print(det, CLASSES[det[-1]])
-            # buff = dumps(dets)
-            # print('nano: sending bytes', len(buff))
-            # conn.sendall(buff)
-            
+                        
 if __name__ == '__main__':
     server = NanoServer()
     while True:
