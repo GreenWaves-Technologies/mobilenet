308e42cebddbb4291e6d53e48c6bedb745e77286
Ben Marlin <marlin@cs.umass.edu>
2022-02-12 13:43:34 -0500

    indicate hostname on server start

--- NanoImageServer/start_img_server.sh
+++ NanoImageServer/start_img_server.sh
@@ -1,5 +1,7 @@
 echo "Starting Apache web server in docker on port 8080"
 docker run --rm -d -p 8080:80 -v "$PWD/htdocs":/usr/local/apache2/htdocs/ httpd:2.4-alpine
 
-echo "Starting image server. Go to localhost:8080/detect_scoket.html to view"
+hostname=`hostname`
+
+echo "Starting image server. Go to $hostname:8080 to view"
 python img_server.py
