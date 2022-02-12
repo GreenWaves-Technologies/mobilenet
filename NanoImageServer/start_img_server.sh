b0b9cb8f6d313591c1d887bbd5bd50cdeddec45e
Colin Samplawski <csamplawski@cs.umass.edu>
2022-02-12 17:01:29 -0500

    added sudo to docker run

--- NanoImageServer/start_img_server.sh
+++ NanoImageServer/start_img_server.sh
@@ -1,7 +1,7 @@
 echo "Starting Apache web server in docker on port 8080"
-docker run --rm -d -p 8080:80 -v "$PWD/htdocs":/usr/local/apache2/htdocs/ httpd:2.4-alpine
+sudo docker run --rm -d -p 8080:80 -v "$PWD/htdocs":/usr/local/apache2/htdocs/ httpd:2.4-alpine
 
 hostname=`hostname`
 
 echo "Starting image server. Go to $hostname:8080 to view"
-python img_server.py
+python3 img_server.py
