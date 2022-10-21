1df1ef70c06e30cb8d8c1af328c112f0b33f96ca
Marco Fariselli <marco.fariselli@greenwaves-technologies.com>
2022-10-21 14:18:29 +0200

    a little bit less mram otherwise it overflow

--- models/test_accuracy_nntool.py
+++ models/test_accuracy_nntool.py
@@ -8,12 +8,14 @@ model_path = sys.argv[1]
 model_name, ext = os.path.splitext(model_path)
 model_name = os.path.split(model_name)[-1]
 dataset_path = sys.argv[2]
-print(f"testing model {model_path} on dataset {dataset_path}")
+print(f"testing model {model_path} on dataset {dataset_path} -> logging to {model_name}")
 
 if len(sys.argv) > 3:
     ne16 = sys.argv[3] == "ne16"
+    fp16 = sys.argv[3] == "fp16"
 else:
     ne16 = True
+    fp16 = False
 
 G = NNGraph.load_graph(model_path, load_quantization=ext == ".tflite")
 if ext != ".json":
@@ -29,4 +31,5 @@ if ext != ".json":
     )
 
 is_ne16 = any(qrec.cache.get("ne16")  for qrec in G.quantization.values())
-test_imagenet_nntool(G, dataset_path, f"{model_name}_nntool{'_ne16' if is_ne16 else '_sw'}.log")
+is_fp16 = any(qrec.ktype == "float"  for qrec in G.quantization.values())
+test_imagenet_nntool(G, dataset_path, f"{model_name}_nntool{'_ne16' if is_ne16 else ('_fp16' if is_fp16 else '_sw')}.log")
